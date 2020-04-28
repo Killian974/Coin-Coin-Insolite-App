7b5c6ac044fef082f613c84d04e7efcd9a7e1c3a
Killian974 <bellour.killian@gmail.com>
2020-04-28 18:28:22 +0200

    Ajout lien vers firebase (bdd)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDjPa2Eg6yFJ61WH1-hlNHc2C8cxklJNqc',
+  authDomain: 'coincoininsolite-d5685.firebaseapp.com',
+  databaseURL: 'https://coincoininsolite-d5685.firebaseio.com',
+  projectId: 'coincoininsolite-d5685',
+  storageBucket: 'coincoininsolite-d5685.appspot.com',
+  messagingSenderId: '381030222565',
+  appId: '1:381030222565:web:22bf6e4d7ce25e31e9220c'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -11,5 +11,4 @@ export class RegisterPage implements OnInit {
 
   ngOnInit() {
   }
-
 }
